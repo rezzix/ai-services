a11a440c136b5844561804086d5742099ca0d6fc
Ossama Boughaba <boughaba@gmail.com>
2024-08-16 11:13:09 +0100

    adding parametrability and a few models

--- test_ollama.py
+++ test_ollama.py
@@ -1,5 +1,5 @@
 import ollama
-
+import time
 
 agent = "Tu es medecin travaillant pour une assurance, ta mission est d'évaluer les prescriptions pour le patient présenté et alerter sur les éventuelles intéractions ou risques de fraude (declaration de prescriptions inutiles ou dangereuses). Pour des raisons de confidentialité, tu ne peux pas demander de renseignements supplémentaires sur le patient ou le prescripteur. "
 task = "Quelles sont les notifications et alerte par rapport à cette prescription ? donnes des réponses courtes et évites les consigne génériques comme vérifiez avec votre médecin, le prescripteur est supposé avoir déjà vérifié les prescriptions et allérgies. Reponses sous forme de liste de points de quatre catégories : 'risques de fraude', 'prescription dangereuse', 'prescription inutile' ou 'hors spécialité' dans cet ordre. n'affiche pas une catégorie si elle ne s'applique pas et ne répète pas le même problème sous deux catégories différentes. "
@@ -9,7 +9,12 @@ case = "cas : " +"fievre et toux sèche. "
 prescriptor = "prescripteur : " + "Traumatologue. "
 prescription = "prescription : Warfarin 5mg, une fois par jour. Ibuprofen 100mg, toutes les  4 à 6 heures au besoin."
 
-stream = ollama.chat(model='llama3', messages=[
+model='mistral-nemo' #'gemma2' 'gemma2:27b' 'ollama3.1'
+
+stream=False
+start_time = time.time()
+
+response = ollama.chat(model=model, messages=[
   {
     'role': 'user',
     'content': agent,
@@ -36,10 +41,21 @@ stream = ollama.chat(model='llama3', messages=[
   },
   
 ],
-stream = True,
-format = 'json',
+stream = stream,
+#format = 'json',
 )
-#print(response['message']['content'])
 
-for chunk in stream:
-  print(chunk['message']['content'], end='', flush=True)
+if stream :
+  for message in response:
+    print(message["message"]["content"], end="", flush=True)
+else :
+  print(response['message']['content'])
+
+
+elapsed_time = time.time() - start_time
+print(f"\nTime taken with {model} model : {elapsed_time:.2f} seconds")
+
+"""
+# Stream the response
+
+"""
